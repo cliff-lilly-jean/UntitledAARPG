fecad7ac73c15d11e9d4bfae705800d0e23ddb18
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2024-10-09 03:55:17 -0400

    added the player

---
+++ Assets/Scripts/Movement/Data/MovementData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Movement/Movement Data", order = 1)]
+public class MovementData : ScriptableObject
+{
+    public float speed;
+    public float maxSpeed;
+}
--- Assets/Scripts/Movement/Move/Move.cs
+++ Assets/Scripts/Movement/Move/Move.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Move : MonoBehaviour
 {
+    public MovementData movementData;
+
+
+    private Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(movementData.maxSpeed);
     }
 }
